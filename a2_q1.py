22ae14346f40ae98da1f766bf6bc2de456f6cc59
knishibe <32073936+knishibe@users.noreply.github.com>
2020-06-07 12:11:35 -0700

    Part 1 and 2 complete with simple tests

---
+++ a2_q1.py
@@ -0,0 +1,21 @@
+﻿import random
+
+def rand_graph(p, n):
+    
+    connected = [[] for i in range(n)]
+    graph = {}
+    random.seed()
+
+    for i in range(n):
+        for j in range(n):
+            r = random.randint(0, 100);
+            if(j > i and r < p*100):
+                connected[i].append(j)
+                connected[j].append(i)
+
+    for i in range(0, n-1):
+        graph.update({str(i) : connected[i]})
+
+    return graph
+
+print(rand_graph(0.5, 4))
---
+++ a2_q2.py
@@ -0,0 +1,27 @@
+﻿def check_teams(graph, csp_sol):
+
+    i = 0
+
+    for key, val in graph.items():
+        team = csp_sol[key]
+        for i in val:
+            if team == csp_sol[str(i)]:
+                return False
+
+    return True
+
+graph1 = {
+    '0' : [1, 2],
+    '1' : [0],
+    '2' : [0],
+    '3' : []
+    }
+
+graph2 = {
+    '0' : 0,
+    '1' : 1,
+    '2' : 2,
+    '3' : 1
+    }
+
+print(str(check_teams(graph1, graph2)))
